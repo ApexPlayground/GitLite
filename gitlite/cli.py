0735a57456ee22db5a8fc56ff3d5d96bfe622f7d
Divine <115173705+ApexPlayground@users.noreply.github.com>
2025-03-17 18:23:36 +0000

    feat: reading the files and hashing it to create unique ids

--- gitlite/cli.py
+++ gitlite/cli.py
@@ -17,9 +17,19 @@ def parse_arguments():
     init_parser = sub_commands.add_parser('init')
     init_parser.set_defaults(func=init)# When 'init' is used, call the 'init' function
 
+    # hash object parser for file
+    hash_object_parser = sub_commands.add_parser ('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument ('file')
+    
     return parser.parse_args()
 
 
 def init(args: any):
     data.init() # calls the init function from data.py
     print(f'Initialized  empty gitlite repository in {os.path.join(os.getcwd(), data.GIT_DIR)}')
+
+def hash_object(args: any):
+    with open(args.file, 'rb') as file:
+        # prints the newly hashed file
+        print(data.hash_object(file.read()))
--- gitlite/data.py
+++ gitlite/data.py
@@ -1,9 +1,34 @@
 import os
+import hashlib
 
 GIT_DIR = '.gitlite'
 
 def init():
     if not os.path.exists(GIT_DIR):
         os.makedirs(GIT_DIR)
+
+        # Create the objects directory inside the .gitlite directory
+        os.makedirs(os.path.join(GIT_DIR, 'objects'), exist_ok=True)
     else:
         print(f'The directory {GIT_DIR} already exist')
+
+def hash_object(data: any):
+
+    # if data is str, convert to byte
+    if isinstance(data, str):
+        data = data.encode('utf-8')
+
+    # Perform sha1 hashing and convert to hexadecimal
+    object_ID = hashlib.sha1(data).hexdigest()
+
+    # path to write tha data to
+    file_path = os.path.join(GIT_DIR, 'objects', object_ID)
+
+     # Ensure the directory exists before writing to it
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    # Write the data to the file
+    with open(file_path, 'wb') as out:
+        out.write(data)
+    
+    return object_ID
